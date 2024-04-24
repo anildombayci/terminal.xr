fa67d8f6b836ac41541467aaf36958c10fcd7fb6
Anıl <142122890+anildombayci@users.noreply.github.com>
2024-04-25 00:52:31 +0300

    Publish Terminal.XR

---
+++ addons/adapter.js
@@ -0,0 +1,11 @@
+/**
+ * Your Custom Logger Schema.
+ */
+module.exports = class Adapter {
+    constructor() {
+        this.name = ""
+        this.filename = ""
+        this.dirname = "" 
+        this.color = ""
+    }
+}
